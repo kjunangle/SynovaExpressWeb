b74e9b9c700e72436689816a65d9d8481ba610f7
Burawat Wangwonsin <mommam456@gmail.com>
2015-08-14 22:14:00 +0700

    Create

---
+++ SynovaExpressWeb/SynovaExpressWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SynovaExpressWeb.Startup))]
+namespace SynovaExpressWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
